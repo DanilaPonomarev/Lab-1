f0c53fbcf9404fc6ab4e25dedf4c3199289980c8
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 10:26:01 +0300

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{ 
+    float y,x,q;
+    y = 0.5;
+    x = 2.0;
+    q = x+y;
+    if (q > x)
+        {
+        printf("ДА");
+        }
+    else
+        {
+        printf("НЕТ");
+        }
+}
