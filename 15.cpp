92d2ac1e53336eef4ff826d01a998030f65e284c
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 10:31:40 +0300

    Create 15.cpp

---
+++ 15.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+  double d = 3.2, x; 
+  int i = 2, y;
+	x = (y = d / i) * 2; 
+	printf("x = %f ;y = %d\n", x, y);
+	x = (y = d / i) * 2; 
+	printf("x = %d ;y = %f\n", x, y);
+	y = (x = d / i) * 2; 
+	printf("x = %f ;y = %d\n", x, y);
+	y = d * (x = 2.5 / d); 
+	printf("x = %f; y = %d\n", x, y);
+	x = d * (y = ((int)2.9 + 1.1) / d); 
+	printf("x = %d y = %f\n", x, y);
+}
