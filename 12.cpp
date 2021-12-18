927f4820c7fe6782363581dd1183d62548e17cb5
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 10:28:36 +0300

    Create 12.cpp

---
+++ 12.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    char c; short s; int i; unsigned u; signed char sc;
+	float f; double d; long ing; unsigned short us; long double ld;
+	int ii; double dd; long double lldd; long ll;
+//int
+	c - s / i;
+//double
+	u * 3 - 3.0 * u - i;
+//int
+	u - us * i;
+//long double
+	(sc + d)* ld;	
+//long
+	(5 * ing - 'a')* (s + u / 2);
+//double
+	(f + 3) / (2.5f - s * 3.14);
+
+}
