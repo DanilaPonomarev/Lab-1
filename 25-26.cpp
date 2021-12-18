fe07e312f5e38927cb8e628a11af9253d7cb9114
DanilaPonomarev <96246197+DanilaPonomarev@users.noreply.github.com>
2021-12-18 10:33:14 +0300

    Create 25-26.cpp

---
+++ 25-26.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+//25 и 26 номер
+int main() {
+    unsigned int stack;
+    char array[] = {'a', 'b', 'c', 'd'};
+    for (int i = 0; i < 4; i++) {
+        stack = stack << 8 | array[i];
+    }
+    stack = ((a << 8 | b) << 8 | c) << 8 | d;
+    printf("%d\n", stack);
+printf("\n");
+    char n;
+    for (int i = 0; i < 4; i++) {
+        n = stack;
+        stack >>= 8;
+         printf("n: %c\n", n);
+         printf("n (int): %d\n", (int)n);
+    }
+ }
